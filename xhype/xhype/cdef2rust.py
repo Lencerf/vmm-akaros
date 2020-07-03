ccf927ce426c84e47f483a73728f75eba516a377
Lencerf <changyuan.lv@Gmail.com>
2020-07-02 20:52:36 -0400

    reimplement cdef2rust

--- xhype/xhype/cdef2rust.py
+++ xhype/xhype/cdef2rust.py
@@ -2,26 +2,39 @@ import sys
 
 str_define = "#define"
 
-c_header_file_path = sys.argv[1]
-with open(c_header_file_path) as f:
-    lines = f.readlines()
-    result = []
-    for line in lines:
-        l = line.strip()
-        if l.startswith(str_define):
-            start = len(str_define) + 1
-            i = start
-            while not l[i].isspace():
-                i += 1
-            def_name = l[start:i]
-            def_val = l[i:]
-            if "ULL" in l:
-                result.append(
-                    "pub const {}:u64 = {};".format(def_name, def_val))
-            else:
-                result.append(
-                    "pub const {}:u32 = {};".format(def_name, def_val))
+c_file_path = sys.argv[1]
+start_line = int(sys.argv[2]) - 1
+end_line = int(sys.argv[3])
 
+
+def get_comment(line, start):
+    start = 0
+    while start < len(line) and not line[start:].startswith("/*"):
+        start += 1
+    end = start
+    while end < len(line) and not line[end:].startswith("*/"):
+        end += 1
+    return line[start:end+2]
+
+
+def get_word(line, start):
+    while start < len(line) and line[start].isspace():
+        start += 1
+    end = start
+    while not line[end].isspace():
+        end += 1
+    word = line[start:end]
+    return word, end
+
+
+with open(c_file_path) as f:
+    lines = f.readlines()
+    for line in lines[start_line:end_line]:
+        if line.startswith(str_define):
+            _, next_index = get_word(line, 0)
+            name, next_index = get_word(line, next_index)
+            value, next_index = get_word(line, next_index)
+            comment = get_comment(line, next_index)
+            print("pub const {}:u64 = {};{}".format(name, value, comment))
         else:
-            result.append(l)
-    print("\n".join(result))
+            print(line, end="")
